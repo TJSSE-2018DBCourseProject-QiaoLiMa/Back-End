870c64f6e06b8f6ca87d8819b9d2489f7196bee5
GreeNO <28252059+NONOisash@users.noreply.github.com>
2018-07-24 11:00:12 +0800

    Add files via upload

--- RSDP/RSDP/Models/Context.cs
+++ RSDP/RSDP/Models/Context.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using System.Data.Common;
+
 using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Migrations;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Migrations.History;
+using System.Data.Entity.ModelConfiguration;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace RSDP.Models
 {
 
@@ -19,9 +24,38 @@ namespace RSDP.Models
         public DbSet<Train> Trains { get; set; }
 
 
+        /// <summary>
+        /// 货运
+        /// </summary>
+        public DbSet<FreightOrder> FreightOrder { get; set; }
+
+        public DbSet<Packge> Packge { get; set; }
+
+        public DbSet<Price> Price { get; set; }
+
+        public DbSet<Transfer> Transfer { get; set; }
+        /// <summary>
+        /// 货运
+        /// </summary>
+
+
+
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            /// <summary>
+            /// 货运
+            /// </summary>
+            modelBuilder.Entity<Price>().Property(t => t.BasePriceOne).HasPrecision(5, 2);
+                                          
+            modelBuilder.Entity<Price>().Property(t => t.BasePriceTwo).HasPrecision(6, 2);
+
+
+
+            /// <summary>
+            /// 货运
+            /// </summary>
 
             modelBuilder.HasDefaultSchema("C##TESTUSER");
 
