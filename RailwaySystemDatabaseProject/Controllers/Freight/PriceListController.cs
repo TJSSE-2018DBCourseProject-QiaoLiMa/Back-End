e74d710198a765e7c637baa7b9e03914aa54754e
GreeNO <28252059+NONOisash@users.noreply.github.com>
2018-07-26 03:45:38 +0800

    Add files via upload

--- RailwaySystemDatabaseProject/Controllers/Freight/PriceListController.cs
+++ RailwaySystemDatabaseProject/Controllers/Freight/PriceListController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,6 +12,89 @@ namespace RailwaySystemDatabaseProject.Controllers
     [Route("PriceList")]
     public class PriceListController : ApiController
     {
+        
+
+        [Route("index")]
+        [HttpPut]
+        public bool ChangePriceTable(Price p)//218韦
+        {
+
+            using (var ctx = new OracleDbContext())
+            {
+                try
+                {
+                    var pr = ctx.Price.Where(x => x.ValuationID == p.ValuationID).FirstOrDefault();
+                     pr.BasePriceOne = p.BasePriceOne;
+                     // pr.BasePriceOneUnit = p.BasePriceOneUnit;
+                    pr.BasePriceTwo = p.BasePriceTwo;
+                    //pr.BasePriceTwoUnit = p.BasePriceTwoUnit;
+                    ctx.SaveChanges();
+
+                    //ctx.Train.Add(train);
+                    //ctx.SaveChanges();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            System.Diagnostics.Debug.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                        }
+                    }
+                }
+                
+            }
+            return true;
+
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// ///////////////////////////////////////////////////////////////////////////////////
+        /// </summary>
+        /// <returns></returns>
+        //[Route("index")]
+        //[HttpGet]
+
+        public IHttpActionResult GetPrice()//216
+        {
+            using (var ctx = new OracleDbContext())
+            {
+                try
+                {
+
+
+                    var P = ctx.Price.ToList();
+
+                    return Json<List<Price>>(P);
+
+                    //ctx.Train.Add(train);
+                    //ctx.SaveChanges();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            System.Diagnostics.Debug.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                        }
+
+
+                    }
+                    return Json<string>("errer");
+                }
+
+            }
+        }
+
+        
 
 
     }
